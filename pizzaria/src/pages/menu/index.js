3923aa1746bd642ddf2d8dbb53f2764f678cca23
dinexNinja <diegosilvasd2006@gmail.com>
2022-06-14 11:21:20 -0300

    arrumei o menu +/-

--- pizzaria/src/pages/menu/index.js
+++ pizzaria/src/pages/menu/index.js
@@ -2,6 +2,11 @@ import './index.scss'
 import '../../common/common.scss'
 import logo from "../../images/logo-header.png"
 import pizzaum from "../../images/4queijos.png"
+import pizzadois from "../../images/alho.png"
+import pizzatres from "../../images/alema.png"
+import pizzaquatro from "../../images/bacon.png"
+import pizzacinco from "../../images/pepperoni.png"
+import pizzaseis from "../../images/doce.png"
 import Rodape from '../../components/footer'
 import Storage from 'local-storage'
 import { useNavigate } from 'react-router-dom'
@@ -50,6 +55,7 @@ export default function Menu() {
         <section className="fundo">
             <h1 className='titulo'> Menu</h1>
 
+            <hr className="linha1"></hr>
             <div className="cards">
                 <div className="card1">
                 <img src={pizzaum} className="pizza"></img>
@@ -58,17 +64,73 @@ export default function Menu() {
 
                 <hr className='linha'></hr>
 
-                <p className="ings"> Queijo Requeijão, Parmesão, Gorgonzola, Mussarela</p>
+                <p className="ings"> Queijos Mussarela, Catupiry, Provolone, Parmesão e azeitonas fatiadas</p>
 
-                <p className="preco"> R$ 59,90</p>
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
             </div>
 
             <div className="card1">
-                <img src={pizzaum} className="pizza"></img>
+                <img src={pizzadois} className="pizza"></img>
+
+                <p className='sabor'> Pizza de Alho</p>
+
+                <hr className='linha'></hr>
+
+                <p className="ings"> Alho, Queijos Mussarela e Parmesão, rodelas de Tomate e Azeitonas fatiadas</p>
+
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
             </div>
 
             <div className="card1">
-                <img src={pizzaum} className="pizza"></img>
+                <img src={pizzatres} className="pizza"></img>
+
+                <p className='sabor'> Pizza Alemã</p>
+
+                <hr className='linha'></hr>
+
+                <p className="ings"> Mussarela, Calabresa moída, Cebola, Parmesão e Azeitonas fatiadas</p>
+
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
+            </div>
+            </div>
+
+            <div className="cards">
+                
+            <div className="card1">
+                <img src={pizzacinco} className="pizza"></img>
+
+                <p className='sabor'> Pizza de Pepperoni</p>
+
+                <hr className='linha'></hr>
+
+                <p className="ings"> Mussarela, Pepperoni, Champignon e Azeitonas fatiadas</p>
+
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
+            </div>
+
+            <div className="card1">
+                <img src={pizzaseis} className="pizza"></img>
+
+                <p className='sabor'> Pizza de Chocolate e Morango</p>
+
+                <hr className='linha'></hr>
+
+                <p className="ings"> Chocolate ao leite e morangos fatiados</p>
+
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
+            </div>
+
+            
+            <div className="card1">
+                <img src={pizzaquatro} className="pizza"></img>
+
+                <p className='sabor'> Pizza de Bacon</p>
+
+                <hr className='linha'></hr>
+
+                <p className="ings"> Mussarela, Bacon, Ovo e Azeitonas fatiadas</p>
+
+                <div className="precos"><p className="preco"> R$ 49,90</p></div>
             </div>
             </div>
            
